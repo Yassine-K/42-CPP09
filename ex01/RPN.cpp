56f654ecf61245551e228f60ac2c937238e62f3c
Yak <yassinekhayri123@gmail.com>
2024-09-21 11:12:31 +0100

    RPN fixed

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -17,35 +17,35 @@ int calculator (std::string exp) {
 		if (op_i != std::string::npos && !i)
 			throw ("Reverse Polish Notation not respected!");
 		if (i && op_i != std::string::npos) {
+			if (nums.size() > 1) {
+				res = nums.front();
+				nums.pop_front();
+				switch (tokens.find(calc_elem[0])) {
+					case 0:
+						res += nums.back();
+						break;
+					case 1:
+						res *= nums.back();
+						break;
+					case 2:
+						res /= nums.back();
+						break;
+					default:
+						res -= nums.back();
+						break;
+				}
+				nums.pop_back();
+				nums.push_front(res);
+			}
+			else
 			op.push_back(tokens[tokens.find(calc_elem[0])]);
 		}
-		else if (i) {
+		else {
 			nums.push_back(std::strtod(const_cast<char *>(calc_elem.c_str()), &endp));
 		}
-		else
-			res = std::strtod(const_cast<char *>(calc_elem.c_str()), &endp);
 		i++;
 	}
-	if (nums.size() != op.size())
+	if (op.size() || nums.size() != 1)
 		throw ("Reverse Polish Notation not respected!");
-	std::list<char>::iterator op_it = op.begin();
-	for (std::list<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
-	// std::cout << "Current op: " << res << *op_it << *it<< std::endl;
-		switch (tokens.find(*op_it)) {
-			case 0:
-				res += *it;
-				break;
-			case 1:
-				res *= *it;
-				break;
-			case 2:
-				res /= *it;
-				break;
-			default:
-				res -= *it;
-				break;
-		}
-		++op_it;
-	}
 	return res;
 }
